db8302c59f573fd40a0cd5bfa11feb542d9785a5
Ashraf Teleb <ashraf.teleb85@gmail.com>
2020-02-19 09:32:51 -0500

    added env var to change export3.html server

--- export.js
+++ export.js
@@ -575,7 +575,7 @@ async function handleRequest(req, res)
 				}, 30000);
 				
 				const page = await browser.newPage();
-				await page.goto('https://www.draw.io/export3.html', {waitUntil: 'networkidle0'});
+				await page.goto((process.env.DRAWIO_SERVER_URL || 'https://www.draw.io') + '/export3.html', {waitUntil: 'networkidle0'});
 
 				const result = await page.evaluate((body) => {
 						return render({
