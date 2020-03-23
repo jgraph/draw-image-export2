26f8c493648e74010c5051a1c5984c019f3ea07e
Ashraf Teleb <ashraf.teleb85@gmail.com>
2020-03-23 12:23:52 -0400

    Fixed lost links in exported PDF due to pdf merging and editing

--- export.js
+++ export.js
@@ -194,6 +194,51 @@ function padNumber(n, width, z)
 	return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
 
+function appendPDFPageFromPDFWithAnnotations(pdfWriter, sourcePDF) 
+{
+    var cpyCxt = pdfWriter.createPDFCopyingContext(sourcePDF);
+    var cpyCxtParser = cpyCxt.getSourceDocumentParser();
+    
+    // for each page
+	for(var i = 0; i < cpyCxtParser.getPagesCount(); ++i) 
+	{
+        // grab page dictionary
+        var pageDictionary = cpyCxtParser.parsePageDictionary(i);
+		
+		if(!pageDictionary.exists('Annots')) 
+		{
+            // no annotation. append as is
+            cpyCxt.appendPDFPageFromPDF(i);            
+        }
+		else 
+		{
+            // this var here will save any reffed objects from the copied annotations object.
+            // they will be written after the page copy writing as to not to disturb the
+            // page object writing itself.
+            var reffedObjects;
+
+			pdfWriter.getEvents().once('OnPageWrite', function(params) 
+			{
+                // using the page write event, write the new annotations. just copy the object
+                // as is, saving any referenced objects for future writes
+                params.pageDictionaryContext.writeKey('Annots');
+                reffedObjects = cpyCxt.copyDirectObjectWithDeepCopy(pageDictionary.queryObject('Annots'))
+            })   
+
+            // write page. this will trigger the event  
+            cpyCxt.appendPDFPageFromPDF(i);
+            
+            // now write the reffed object (should be populated cause onPageWrite was written)
+            // note that some or all annotations may be embedded, in which case this array
+            // wont hold all annotation objects
+			if(reffedObjects && reffedObjects.length > 0)
+			{
+				cpyCxt.copyNewObjectsForDirectObject(reffedObjects)
+			}
+        }
+    }
+}
+
 function mergePdfs(pdfFiles, xml)
 {
 	//Pass throgh single files
@@ -220,7 +265,7 @@ function mergePdfs(pdfFiles, xml)
 
 		for (var i = 0; i < pdfFiles.length; i++)
 		{
-			pdfWriter.appendPDFPagesFromPDF(new hummus.PDFRStreamForBuffer(pdfFiles[i]));
+			appendPDFPageFromPDFWithAnnotations(pdfWriter, new hummus.PDFRStreamForBuffer(pdfFiles[i]))
 		}
 
 		pdfWriter.end();
