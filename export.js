398b16b917ae254ca4a2af59127aaa9854c73da0
Ashraf Teleb <ashraf.teleb85@gmail.com>
2022-01-06 15:11:51 -0500

    Added support for svg export with inkscape to convert text to svg without using foreign objects or embedding fonts

--- export.js
+++ export.js
@@ -7,6 +7,10 @@ const zlib = require('zlib');
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const crc = require('crc');
 const PDFDocument = require('pdf-lib').PDFDocument;
+const fs = require("fs").promises;
+const os = require("os");
+const path = require("path");
+const childProcess = require('child_process');
 
 const MAX_AREA = 15000 * 15000;
 const PNG_CHUNK_IDAT = 1229209940;
@@ -183,6 +187,76 @@ function writePngWithText(origBuff, key, text, compressed, base64encoded)
 		throw e;
 	}
 }
+
+//From https://advancedweb.hu/secure-tempfiles-in-nodejs-without-dependencies/
+const withTempFile = (fn) => withTempDir((dir) => fn(path.join(dir, "file")));
+
+const withTempDir = async (fn) => {
+	const dir = await fs.mkdtemp(await fs.realpath(os.tmpdir()) + path.sep);
+	try {
+		return await fn(dir);
+	}finally {
+		fs.rm(dir, {recursive: true});
+	}
+};
+
+function execFile(binPath, args)
+{
+	return new Promise(function (resolve, reject) 
+	{
+		childProcess.execFile(binPath, args, {
+			timeout: 25000 //25 sec
+		}, (error, stdout, stderr) =>
+		{
+			if (error) 
+			{
+				reject(error);
+			}
+			else
+			{
+				resolve(stdout);
+			}
+		});
+	});
+};
+
+async function pdfToSvg(pdfFile, xml, transparent)
+{
+	return await withTempFile(async (tmpFile) => 
+	{
+		await fs.writeFile(tmpFile + '.pdf', pdfFile);
+		
+		await execFile(process.env.INKSCAPE_PATH || 'inkscape', 
+			[tmpFile + '.pdf',
+			'--pdf-poppler',
+			'--export-text-to-path', 
+			'--export-plain-svg=' + tmpFile + '.svg']); //TODO --export-plain-svg= is deprecated, cannot find the alternative yet
+	
+		let svg = await fs.readFile(tmpFile + '.svg', 'utf8');
+
+		if (xml)
+		{
+			//TODO Is this string manubulation safe? DOM parsing is not easily done with JSDOM 
+			/*
+			const dom = new JSDOM(svg, {
+				contentType: 'text/xml'
+			});
+			
+			doc.documentElement.setAttribute('content', xml);
+			svg = dom.serialize();*/
+
+			svg = svg.replace(/<svg([^>]*)>/, '<svg $1 content="' + encodeURIComponent(xml.replace(/\n|\r/g, ' ')) + '">');
+		}
+
+		if (transparent)
+		{
+			//TODO Check that the first element is always the background, also, it has the same style
+			svg = svg.replace('style="fill:#ffffff;fill-opacity:1;stroke:none"', 'style="fill:#ffffff;fill-opacity:0;stroke:none"');
+		}
+		
+		return svg;
+	});
+};
 
 async function mergePdfs(pdfFiles, xml)
 {
@@ -584,10 +658,12 @@ async function handleRequest(req, res)
 					
 					logger.info("Success " + reqStr + " dt=" + dt);
 				}
-				else if (req.body.format == 'pdf')
+				else if (req.body.format == 'pdf' || req.body.format == 'svg')
 				{
+					var isSVG = req.body.format == 'svg';
 					var from = req.body.allPages? 0 : parseInt(req.body.from || 0);
-					var to = req.body.allPages? 1000 : parseInt(req.body.to || 1000) + 1; //The 'to' will be corrected later
+					var to = isSVG? from + 1: //SVG is for one page only
+							 (req.body.allPages? 1000 : parseInt(req.body.to || 1000) + 1); //The 'to' will be corrected later
 					var pageId;
 					var pdfs = [];
 
@@ -604,7 +680,9 @@ async function handleRequest(req, res)
 						pdfs.push(await page.pdf(info.pdfOptions));
 					}
 
-					var data = await mergePdfs(pdfs, req.body.embedXml == '1' ? xml : null);
+					var data = isSVG? await pdfToSvg(pdfs[0], req.body.embedXml == '1' ? xml : null, 
+														req.body.bg == null || req.body.bg == 'none') :
+									await mergePdfs(pdfs, req.body.embedXml == '1' ? xml : null);
 
 					if (req.body.filename != null)
 					{
@@ -617,7 +695,7 @@ async function handleRequest(req, res)
 						data = data.toString('base64');
 					}
 					
-					res.header('Content-type', base64encoded? 'text/plain' : 'application/pdf');
+					res.header('Content-type', isSVG? 'image/svg+xml' : (base64encoded? 'text/plain' : 'application/pdf'));
 					res.header("Content-Length", data.length);
 					
 					if (pageId != null && pageId != 'undefined')
