c7c56aade72e284b6e8b077cf65dad5e575da169
Mohamed Mohamedin <mohamedin@draw.io>
2024-03-07 22:23:36 -0500

    Added more optimization to puppeteer instances

--- export.js
+++ export.js
@@ -43,6 +43,45 @@ if (!NO_CLUSTER && cluster.isMaster)
 }
 else
 {
+	const minimal_args = [
+		'--disable-gpu',
+		'--autoplay-policy=user-gesture-required',
+		'--disable-background-networking',
+		'--disable-background-timer-throttling',
+		'--disable-backgrounding-occluded-windows',
+		'--disable-breakpad',
+		'--disable-client-side-phishing-detection',
+		'--disable-component-update',
+		'--disable-default-apps',
+		'--disable-dev-shm-usage',
+		'--disable-domain-reliability',
+		'--disable-extensions',
+		'--disable-features=AudioServiceOutOfProcess',
+		'--disable-hang-monitor',
+		'--disable-ipc-flooding-protection',
+		'--disable-notifications',
+		'--disable-offer-store-unmasked-wallet-cards',
+		'--disable-popup-blocking',
+		'--disable-print-preview',
+		'--disable-prompt-on-repost',
+		'--disable-renderer-backgrounding',
+		'--disable-setuid-sandbox',
+		'--disable-speech-api',
+		'--disable-sync',
+		'--hide-scrollbars',
+		'--ignore-gpu-blacklist',
+		'--metrics-recording-only',
+		'--mute-audio',
+		'--no-default-browser-check',
+		'--no-first-run',
+		'--no-pings',
+		'--no-sandbox',
+		'--no-zygote',
+		'--password-store=basic',
+		'--use-gl=swiftshader',
+		'--use-mock-keychain',
+	];
+
 	const MAX_AREA = 20000 * 20000;
 	const PNG_CHUNK_IDAT = 1229209940;
 	const { JSDOM } = require("jsdom");
@@ -332,7 +371,8 @@ else
 					
 					browser = await puppeteer.launch({
 						headless: 'chrome-headless-shell',
-						args: ['--disable-gpu', '--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
+						args: minimal_args,
+						userDataDir: './puppeteer_user_data'
 					});
 					
 					// Workaround for timeouts/zombies is to kill after 30 secs
@@ -510,7 +550,8 @@ else
 						
 						browser = await puppeteer.launch({
 							headless: 'chrome-headless-shell',
-							args: ['--disable-gpu', '--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
+							args: minimal_args,
+							userDataDir: './puppeteer_user_data'
 						});
 
 						// Workaround for timeouts/zombies is to kill after 30 secs
